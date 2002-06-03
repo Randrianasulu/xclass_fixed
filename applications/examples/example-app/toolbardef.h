aa7fd2fdbc5e1800f304482aaeaa3a79bec965a6
hperaza <>
2002-06-03 14:31:20 +0000

    Corrected tooltip text for 'save' button

--- applications/examples/example-app/toolbardef.h
+++ applications/examples/example-app/toolbardef.h
@@ -6,11 +6,11 @@
 //---------- toolbar button definitions
 
 SToolBarData tb_data[] = {
-  { "tb-open.xpm",  tb_open_xpm,  "Open File",         BUTTON_NORMAL, M_FILE_OPEN,  NULL },
-  { "tb-save.xpm",  tb_save_xpm,  "Save Marked Pages", BUTTON_NORMAL, M_FILE_SAVE,  NULL },
-  { "tb-print.xpm", tb_print_xpm, "Print",             BUTTON_NORMAL, M_FILE_PRINT, NULL },
-  { "",             NULL,         0,                   0,             -1,           NULL },
-  { NULL,           NULL,         NULL,                0,             0,            NULL }
+  { "tb-open.xpm",  tb_open_xpm,  "Open File", BUTTON_NORMAL, M_FILE_OPEN,  NULL },
+  { "tb-save.xpm",  tb_save_xpm,  "Save File", BUTTON_NORMAL, M_FILE_SAVE, NULL },
+  { "tb-print.xpm", tb_print_xpm, "Print",     BUTTON_NORMAL, M_FILE_PRINT, NULL },
+  { "",             NULL,         0,           0,             -1,           NULL },
+  { NULL,           NULL,         NULL,        0,             0,            NULL }
 };
 
 #endif  // __TOOLBARDEF_H
