fef96d60f7dafcb2cad13db1d90ec3478fa2560a
hperaza <>
2002-06-03 13:38:33 +0000

    Updated release date

--- xclass/include/xclass/version.h
+++ xclass/include/xclass/version.h
@@ -27,7 +27,7 @@
 #define XCLASS_MINOR_VERSION  "6"
 #define XCLASS_PATCH_LEVEL    "3"
 #define XCLASS_VERSION        "0.6.3"
-#define XCLASS_RELEASE_DATE   "21-Mar-2002"
+#define XCLASS_RELEASE_DATE   "3-Jun-2002"
 
 
 #endif // __VERSION_H
