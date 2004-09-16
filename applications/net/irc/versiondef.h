01678c3c6e73769ed2630133733681745c00a7ca
hperaza <>
2004-09-16 15:45:27 +0000

    - updated version number

--- applications/net/irc/versiondef.h
+++ applications/net/irc/versiondef.h
@@ -2,8 +2,8 @@
 #define __VERSIONDEF_H
 
 #define FOXIRC_NAME           "fOXIrc"
-#define FOXIRC_VERSION        "0.4.3"
-#define FOXIRC_RELEASE_DATE   "15-May-2003"
+#define FOXIRC_VERSION        "0.4.4"
+#define FOXIRC_RELEASE_DATE   "16-Sep-2004"
 #define FOXIRC_HOMEPAGE       "http://xclass.sourceforge.net"
 
 #endif  // __VERSIONDEF_H
